cbc5fe05a54134b32fad09421fe29263b58a006d
Eric Austin <eaustin@gnresound.com>
2020-01-14 08:41:57 -0600

    Updated

--- Script.ps1
+++ Script.ps1
@@ -29,13 +29,26 @@ $Command2=$Connection2.CreateCommand()
 
 Try {
 
+    Clear-Host
+    Write-Host ""
+
     #ensure the SqlServer module is installed
+    Write-Host "Checking for SqlServer module..."
     if (-Not (Get-Module -ListAvailable -Name "SqlServer")){
         Throw "The SqlServer module is required to run this script. Run `"Install-Module -Name SqlServer -Scope CurrentUser`" to install the module and then run the script again."
     }
+    Write-Host "SqlServer module is installed"
 
     #retrieve server list from environmental variable
-    $ServerList=$Env:SQLServerList.Split(",")
+    Write-Host "Retrieving SQL Server list from environmental variable..."
+    if ([string]::IsNullOrWhiteSpace($Env:SQLServerList))
+    {
+        Throw "The environmental variable `"SqlServerList`" either does not exist or is empty, exiting now."
+    }
+    else
+    {
+        $ServerList=$Env:SQLServerList.Split(",")
+    }
 
     #receive server selection
     $Server=($ServerList | Sort-Object | Out-GridView -PassThru)
@@ -50,6 +63,7 @@ Try {
     #set backup directory
     #this gets the location of the last backup for the database
     #if no backup for the database exists in the directory throw an error
+    Write-Host "Looking up backup location..."
     $Connection1.ConnectionString="Server=$($Server); Database=msdb; Trusted_Connection=true;"
     $Command1.CommandText="
         SELECT TOP 1 f.physical_device_name
@@ -68,6 +82,7 @@ Try {
     $BackupFileName="$($Database)_$(Get-Date -Format "MMddyyyy_hhmmss")_$((New-Guid).Guid).bak"
 
     #create new backup
+    Write-Host "Creating new backup..."
     Backup-SqlDatabase -ServerInstance $Server -Database $Database -BackupFile (Join-Path -Path $BackupFileDirectory -ChildPath $BackupFileName)
 
     #set local backup directory
@@ -80,9 +95,15 @@ Try {
 
     #robocopy the backup to the new location (use instead of move-item because if I don't have permission to delete from the backup file directory robocopy still does the copy but not the delete; move-item just fails)
     #if I have permissions to delete from the backup file directory then /mov will delete the file after copying it
+    Write-Host "Copying backup to local backup location..."
     robocopy "$($BackupFileDirectory)" "$($LocalBackupFileDirectory)" "$($BackupFileName)" /mov
+    if ($LASTEXITCODE -ne 1)
+    {
+        Throw "Robocopy did not return a success code of 1, it returned a code of $($LASTEXITCODE). See https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/robocopy#exit-return-codes. Exiting..."
+    }
 
     #drop existing local database
+    Write-Host "Dropping local database if it exists..."
     $Command2.CommandText="
         IF EXISTS (SELECT * FROM sys.databases WHERE name='$($Database)')
         BEGIN
@@ -91,7 +112,10 @@ Try {
     $Command2.ExecuteNonQuery() | Out-Null
 
     #restore backup locally
+    Write-Host "Restoring backup locally..."
     Restore-SqlDatabase -ServerInstance . -Database $Database -BackupFile (Join-Path -Path $LocalBackupFileDirectory -ChildPath $BackupFileName) -AutoRelocateFile
+
+    Write-Host "Success."
 
 }
 
